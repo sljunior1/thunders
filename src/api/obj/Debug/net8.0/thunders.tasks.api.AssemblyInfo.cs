ea0ef34641278cf4f991aa1d4ebd4957e8fae9f8
Silas Lima da Silva Júnior <silas.junior@ltm.digital>
2024-01-15 21:19:14 -0300

    Atualização

--- src/api/obj/Debug/net8.0/thunders.tasks.api.AssemblyInfo.cs
+++ src/api/obj/Debug/net8.0/thunders.tasks.api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("thunders.tasks.api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdc228a6948ad49b27378915460fb96d00597e2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6292ea13628fe34bf366256f2ff9731b344397a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("thunders.tasks.api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("thunders.tasks.api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
